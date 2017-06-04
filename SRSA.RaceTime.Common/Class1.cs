ff61d2431a9ae0e1f1bc65465badf8ce98e59c94
Eckardt Briedenhann <maxchal72@gmail.com>
2017-06-04 16:01:46 +0200

    Projects Created.

---
+++ SRSA.RaceTime.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SRSA.RaceTime.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ SRSA.RaceTime.CoreAPI/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SRSA.RaceTime.Common;
+
+namespace SRSA.RaceTime.CoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Class1 class1 = new Class1();
+            return new string[] { "value1", "value2", "value3", class1.ToString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
